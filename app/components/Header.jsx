9ee77032145262e5a0157aa2935d2d6cacc93b7e
Franci Branda-CHen <fbc101@seas.upenn.edu>
2025-03-30 14:26:39 -0400

    added title

---
+++ app/components/Header.jsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+  return (
+    <header>
+      <h1 className="font-italianno text-7xl text-title text-center pt-10">Mangia Mogo</h1>
+    </header>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Header from "./components/Header";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-black font-bold">
+      hello world
+    </div>
+  );
+}
