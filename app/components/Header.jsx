07b8f6d659cdcc2ed90f21fceb02c761e792f151
Franci Branda-CHen <fbc101@seas.upenn.edu>
2025-03-30 19:38:56 -0400

    made title header navigable

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header>
-      <h1 className="font-italianno text-7xl text-title text-center pt-5 pb-5 w-full">Mangia Mogo</h1>
+      <Link href="/">
+        <h1 className="font-italianno text-7xl text-title text-center pt-5 pb-5 w-full">Mangia Mogo</h1>
+      </Link>
     </header>
   );
 }
