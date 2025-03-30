b3a09db3d08d56aaf7706beabd59be40244ee525
Franci Branda-CHen <fbc101@seas.upenn.edu>
2025-03-30 17:54:31 -0400

    search bar modification

--- app/components/SearchBar.jsx
+++ app/components/SearchBar.jsx
@@ -4,11 +4,12 @@ import Image from "next/image";
 import searchIcon from "../assets/Search.svg";
 import { useRef } from 'react'; 
 
-export default function SearchBar() {
+export default function SearchBar({ onSearch }) {
     const inputRef = useRef(null); 
 
     const handleSearch = () => {
-        if (inputRef.current) {
+        if (inputRef.current && onSearch) {
+            onSearch(inputRef.current.value);
             console.log(inputRef.current.value);
         }
     };
@@ -20,14 +21,14 @@ export default function SearchBar() {
     };
 
     return (
-        <div className="flex w-3/4">
-            <h1 className="flex justify-center items-center pl-2 pr-2 text-3xl font-bold text-title">
+        <div className="flex w-3/4 p-2">
+            <h1 className="flex justify-center items-center pl-2 pr-4 text-3xl font-bold text-title">
                 Search
             </h1>
             <input
                 type="text"
                 placeholder="Type the dish you want to cook"
-                className="w-full pl-2 rounded-full border border-gray-300 bg-search"
+                className="w-full pl-4 rounded-full border border-gray-300 bg-search"
                 ref={inputRef} 
                 onKeyDown={handleKeyDown}
             />
--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,10 +1,18 @@
+'use client';
+
+import { useState } from 'react'; 
 import SearchBar from "../components/SearchBar";
 
 export default function SearchPage() {
+    const [searchText, setSearchText] = useState(''); 
+
     return (
-      <div className="flex flex-col items-center justify-center flex-grow text-black font-bold">
-        <h1>Search page</h1>
-        <SearchBar/>
-      </div>
+        <div className="flex flex-col items-center justify-center flex-grow text-black text-2xl">
+            <h1>Search page</h1>
+            <SearchBar onSearch={setSearchText} /> 
+            <div className="flex flex-col items-center justify-center flex-grow">
+                {searchText && <p>You searched for: {searchText}</p>} 
+            </div>
+        </div>
     );
-  }
+}
