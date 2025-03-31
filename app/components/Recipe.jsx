9d1c1d3191ade23ea7ffa63265096f88e064303d
Franci Branda-CHen <fbc101@seas.upenn.edu>
2025-03-31 13:35:22 -0400

    fix of recipe misalignment

--- app/components/Recipe.jsx
+++ app/components/Recipe.jsx
@@ -1,29 +1,34 @@
-import Link from "next/link"; 
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Recipe({ name, ingredients, description, username, image, difficulty, country, price }) {
-    const sanitizedUsername = username.toLowerCase().replace(/[^a-z0-9-]+/g, '-');
-    const sanitizedRecipe = name.toLowerCase().replace(/[^a-z0-9-]+/g, '-');
+  const sanitizedUsername = username.toLowerCase().replace(/[^a-z0-9-]+/g, '-');
+  const sanitizedRecipe = name.toLowerCase().replace(/[^a-z0-9-]+/g, '-');
 
-
-    return (
-        <div className="flex flex-row justify-center p-5">
-            <Link href={`/search/${sanitizedUsername}/${sanitizedRecipe}`}>
-                <Image src={image} alt="recipe" className=" cursor-pointer" />
-            </Link>
-            <div className="justify-self-start pl-10 max-w-xl">
-                <Link href={`/search/${sanitizedUsername}/${sanitizedRecipe}`}>
-                    <h1 className="text-2xl font-bold cursor-pointer">{name}</h1>
-                </Link>
-                <Link href={`/${sanitizedUsername}`}>
-                    <p className="text-sm pb-4">by {username}</p>
-                </Link>
-                <p>Ingredients: {ingredients}</p>
-                <p>Price: ${price}</p>
-                <p>Difficulty: {difficulty}</p>
-                <p>Country: {country}</p>
-                <p className="text-lg">{description}</p>
-            </div>
+  return (
+    <div className="flex flex-row justify-center p-5">
+      <Link href={`/search/${sanitizedUsername}/${sanitizedRecipe}`}>
+        <div className="w-[300px] h-[300px]">
+          <Image
+            src={image}
+            alt="recipe"
+            className="rounded-xl w-full h-full object-cover object-center cursor-pointer" // Add object-center
+          />
         </div>
-    );
+      </Link>
+      <div className="justify-self-start pl-10 w-xl">
+        <Link href={`/search/${sanitizedUsername}/${sanitizedRecipe}`}>
+          <h1 className="text-2xl font-bold cursor-pointer">{name}</h1>
+        </Link>
+        <Link href={`/${sanitizedUsername}`}>
+          <p className="text-sm pb-4">by {username}</p>
+        </Link>
+        <p>Ingredients: {ingredients}</p>
+        <p>Price: ${price}</p>
+        <p>Difficulty: {difficulty}</p>
+        <p>Country: {country}</p>
+        <p className="text-lg">{description}</p>
+      </div>
+    </div>
+  );
 }
--- app/search/page.jsx
+++ app/search/page.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import SearchBar from "../components/SearchBar";
 import Recipe from "../components/Recipe";
 import burger from "../assets/Burger.svg";
+import cookie from "../assets/Choco_cookie.jpg";
 
 export default function SearchPage() {
     const [searchText, setSearchText] = useState(''); 
@@ -15,17 +16,34 @@ export default function SearchPage() {
             <div className="flex items-center justify-center flex-grow">
                 {searchText && <p>You searched for: {searchText}</p>} 
             </div>
-            {searchText && (searchText.toLowerCase().startsWith('c') || searchText.toLowerCase().includes('chicken')) && (
-                <Recipe name="Chicken Burger" 
-                    ingredients="ground chicken, lettuce, tomato" 
-                    description="Perfect for a quick lunch. It's easy to make and tastes great. My grandma used to make this when I was a kid." 
-                    username="Gordon Ramsay"
-                    image={burger}
-                    price={10}
-                    difficulty="easy"
-                    country="USA"
-                />
-            )}
+            <div className="flex flex-col items-center justify-center flex-grow">
+                {searchText && (searchText.toLowerCase().startsWith('c') || searchText.toLowerCase().includes('chicken')) && (
+                    <div className="flex flex-row items-center justify-center flex-grow">
+                        <Recipe name="Chicken Burger" 
+                            ingredients="ground chicken, lettuce, tomato" 
+                            description="Perfect for a quick lunch. It's easy to make and tastes great. My grandma used to make this when I was a kid." 
+                            username="Gordon Ramsay"
+                            image={burger}
+                            price={10}
+                        difficulty="easy"
+                            country="USA"
+                        />
+                    </div>
+                )}
+                {searchText && (searchText.toLowerCase().startsWith('c') || searchText.toLowerCase().includes('cookie')) && (
+                    <div className="flex flex-row items-center justify-center flex-grow">
+                        <Recipe name="Chocolate Cookie" 
+                        ingredients="milk, flour, eggs, sugar, cocoa" 
+                        description="Just how grandma used to make them" 
+                        username="granny"
+                            image={cookie}
+                            price={10}
+                            difficulty="easy"
+                            country="USA"
+                        />
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
