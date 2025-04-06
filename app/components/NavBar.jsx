d892749770b37efb3de7dfcfa1ca7d9874a0df08
Franci Branda-CHen <fbc101@seas.upenn.edu>
2025-04-05 23:30:09 -0400

    small fixes

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -10,18 +10,18 @@ export default function NavBar() {
   const router = useRouter();
 
   return (
-    <div className="flex justify-center items-center w-screen bg-nav h-20 flex-shrink-0">
+    <div className="flex justify-center items-center w-screen bg-nav h-30 flex-shrink-0">
       <div className="flex justify-center items-center pl-6 pr-6" onClick={() => router.push('/')}>
-        <Image src={homeIcon} className="w-12 h-12" alt="Home Icon" />
+        <Image src={homeIcon} className="w-18 h-18" alt="Home Icon" />
       </div>
       <div className="flex justify-center items-center pl-6 pr-6" onClick={() => router.push('/search')}>
-        <Image src={searchIcon} className="w-14 h-14" alt="Search Icon" />
+        <Image src={searchIcon} className="w-18 h-18" alt="Search Icon" />
       </div>
       <div className="flex justify-center items-center pl-6 pr-6" onClick={() => router.push('/message')}>
-        <Image src={messageIcon} className="w-14 h-14" alt="Message Icon" />
+        <Image src={messageIcon} className="w-18 h-18" alt="Message Icon" />
       </div>
       <div className="flex justify-center items-center pl-6 pr-6" onClick={() => router.push('/profile')}>
-        <Image src={profileIcon} className="w-12 h-12" alt="Profile Icon" />
+        <Image src={profileIcon} className="w-16 h-16" alt="Profile Icon" />
       </div>
     </div>
   );
