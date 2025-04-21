b1cbbc8dfdf824f77962b0c26de4a24a7306675f
JillRay973 <143532365+JillRay973@users.noreply.github.com>
2025-04-21 13:33:24 -0400

    app/data/ingredients.js

---
+++ app/data/ingredients.js
@@ -0,0 +1,47 @@
+export const commonIngredients = {
+    Proteins: [
+        "ground chicken",
+        "ground beef",
+        "shrimp",
+        "lamb",
+        "eggs",
+        "tofu",
+        "chicken breast",
+        "salmon"
+    ],
+    Vegetables: [
+        "lettuce",
+        "tomato",
+        "onions",
+        "peppers",
+        "carrots",
+        "broccoli",
+        "spinach",
+        "mushrooms"
+    ],
+    Dairy: [
+        "milk",
+        "butter",
+        "cheese",
+        "yogurt",
+        "cream",
+        "sour cream"
+    ],
+    Pantry: [
+        "flour",
+        "sugar",
+        "rice",
+        "pasta",
+        "bread crumbs",
+        "olive oil",
+        "soy sauce",
+        "spices"
+    ],
+    Condiments: [
+        "mayonnaise",
+        "ketchup",
+        "mustard",
+        "hot sauce",
+        "vinegar"
+    ]
+}; 
